94967af3d41f0d5149f57bcaed6448fea0ffc92a
nived-s <nivedsreenilayam@gmail.com>
2024-04-22 14:48:48 +0530

    door gpio added

--- servo_init.py
+++ servo_init.py
@@ -1,15 +1,31 @@
 from gpiozero import Servo
 from time import sleep
 
-servo = Servo(17)
- 
- 
+servo = Servo(25)
+
+def rotate_0_to_90():
+    val = -1
+    try:
+        while val <= 0:
+            servo.value = val
+            sleep(0.1)
+            val += 0.1
+    except KeyboardInterrupt:
+        print("Rotation stopped")
+
+def rotate_90_to_0():
+    val = 0
+    try:
+        while val >= -1:
+            servo.value = val
+            sleep(0.1)
+            val -= 0.1
+    except KeyboardInterrupt:
+        print("Rotation stopped")
+
 try:
     while True:
-        servo.min()
-        sleep(0.5)
-        servo.mid()
-        sleep(0.5)
-        
+        rotate_0_to_90()
+        rotate_90_to_0()
 except KeyboardInterrupt:
-    print("oh noo")
+    print("Program stopped")
